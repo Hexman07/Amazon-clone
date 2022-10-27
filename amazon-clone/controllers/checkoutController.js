dd984fadab78a608b9285c7fc8591375907731f9
Aditya <adikpote@gmail.com>
2022-10-27 15:39:09 +0530

    Modified checkoutController

--- amazon-clone/controllers/checkoutController.js
+++ amazon-clone/controllers/checkoutController.js
@@ -1,3 +1,21 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+const checkout = async (req, res) => {
+  const { totalPrice } = req.body;
+  try {
+    await stripe.paymentIntents.create({
+      amount: totalPrice * 100,
+      currency: "INR",
+      payment_method: "pm_card_amex_threeDSecureNotSupported",
+      confirm: true,
+      currency: "INR",
+    });
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ success: false });
+  }
+};
+
 module.exports = { checkout };
