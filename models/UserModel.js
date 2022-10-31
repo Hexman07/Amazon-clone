6b8a9a27aebb21a8dd193981912642770e3280b8
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-10-31 13:25:15 +0530

    [Feat]: Added user schema prototype method  to generate jwt

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = mongoose.Schema({
   name: String,
@@ -6,4 +7,10 @@ const UserSchema = mongoose.Schema({
   password: String,
 });
 
-module.exports = mongoose.model("User", UserSchema);
+UserSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+module.exports = mongoose.model('User', UserSchema);
